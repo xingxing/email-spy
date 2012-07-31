585ebce0988ed029188334e8be8ef72613715c4b
Wade Xing <iamxingxing@gmail.com>
2012-07-31 21:25:04 +0800

    better rspec

--- spec/hotmail_spec.rb
+++ spec/hotmail_spec.rb
@@ -2,18 +2,5 @@
 require 'spec_helper'
 
 describe "Hotmail" do
-  before do
-    @test_data = YAML::load(File.read(File.expand_path("spec/fixture/hotmail.yml")))
-    @contacts = EmailSpy.fetch(@test_data["invitor email"], @test_data["invitor password"], 'hotmail')
-  end
-
-  it "应该 得到联系人列表" do
-    @contacts.first.name.should == @test_data['contact']['name']
-    @contacts.first.email.should == @test_data['contact']['email']
-    @contacts.size.should == @test_data['contacts count']
-  end
-
-  it "不应 包含邀请人的电邮地址" do
-    @contacts.map{|c| c.email }.should_not include @test_data['invitor_email']
-  end
+  it_should_behave_like "fetch A contact list","hotmail"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,31 @@
+# -*- coding: utf-8 -*-
 $:.unshift File.expand_path('../../lib', __FILE__)
 
 require 'rubygems'
 require 'bundler/setup'
 
 require 'email-spy'
+
+shared_examples_for "fetch A contact list" do |email_type|
+  let(:expected_result) do 
+    YAML::load(File.read(File.expand_path("spec/fixture/#{email_type}.yml")))
+  end
+  
+  let(:contacts) do 
+    EmailSpy.fetch(expected_result["invitor email"], expected_result["invitor password"], email_type)
+  end
+  
+  it "should return expected contact info" do
+    contacts.should include do |contact|
+      contact.name == expected_result['contact']['name'] and contact.email == expected_result['contact']['email']
+    end
+  end
+
+  it "should return expected length" do
+    contacts.size.should == expected_result['contacts count']
+  end
+
+  it "should not include the invitor's email" do
+    contacts.map{|c| c.email }.should_not include expected_result['invitor_email']
+  end
+end
